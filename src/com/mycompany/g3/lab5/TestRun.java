5f40c0a1e8cec5fac2459a51f737fe6d8a519705
user <user@DESKTOP-T0O7GHA>
2022-07-17 21:26:54 +0700

    First Commit / Build Person Class / Build Student Class / Build TestRun Class

--- src/com/mycompany/g3/lab5/TestRun.java
+++ src/com/mycompany/g3/lab5/TestRun.java
@@ -12,10 +12,10 @@ public class TestRun {
     
     public static void main (String [] args) {
         
- //   System.out.println ("========================== Staff ============================");    
+    System.out.println ("========================== Staff ============================");    
     
- //   Staff ck = new Staff("Chanakarn Kingkaew","85/1 PIM Nonthaburi","PIM",1_000_000);
- //   System.out.println(ck);
+    Staff ck = new Staff("Chanakarn Kingkaew","85/1 PIM Nonthaburi","PIM",1_000_000);
+    System.out.println(ck);
  
    System.out.println ("========================== Student ============================");
     
