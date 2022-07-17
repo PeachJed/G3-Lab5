91cf0b12402078cba3f879a92c43fbc9ca61f40e
user <user@DESKTOP-T0O7GHA>
2022-07-17 19:42:58 +0700

    Insert Hello World

---
+++ src/comycompany/g3/lab5/G3Lab5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comycompany.g3.lab5;
+
+/**
+ *
+ * @author user
+ */
+public class G3Lab5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
